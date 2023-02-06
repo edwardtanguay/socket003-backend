acaaf05cd8ab2cc215fd1720536d662ff8e127eb
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-06 23:23:36 +0100

    backend set up

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,39 @@ import { Server } from 'socket.io';
 
 const app = express();
 app.use(cors());
+const server = http.createServer(app);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	res.send('test')
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(config.port, () => {
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send(`
+<h1>Socket.io Server</h1>	
+<p>This backend connects socket.io clients and enables communication between them.</p>
+	`)
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(config.port, () => {
 	console.log(`listening on port http://localhost:${config.port}`);
 });
